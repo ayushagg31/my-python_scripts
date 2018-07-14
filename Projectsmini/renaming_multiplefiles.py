345206489115522e0e153c5ca0e32a49538f86c1
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2018-07-14 21:05:35 +0530

    Delete renaming_multiplefiles.py

--- Projectsmini/renaming_multiplefiles.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_list = os.listdir(r"/home/ayush/PycharmProjects/untitled/prank/prank")
-cwd = os.getcwd()
-print(cwd)
-os.chdir(r"/home/ayush/PycharmProjects/untitled/prank/prank")
-for file_name in file_list:
-    os.rename(file_name,file_name.translate(None,"0123456789"))
-os.chdir(cwd)
