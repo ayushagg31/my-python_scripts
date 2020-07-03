f3f5bd15f0e616456185d0a8451eeddbb1b80b18
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-07-03 12:17:54 +0530

    Update instaCommenter.py

--- Projectsmini/instaCommenter.py
+++ Projectsmini/instaCommenter.py
@@ -1,3 +1,6 @@
+# Python bot for logging in and commenting on Instagram Post.
+# Tech Used: Selenium 
+
 # coding: utf-8
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -26,20 +29,20 @@ def open_comment_section(browser):
         print(missing_comment_elem_warning)
 
 
-browser = webdriver.Chrome("/home/gozmit/Downloads/chromedriver") 
+browser = webdriver.Chrome(############) 	# add the path of your chrome web driver
 browser.get('https://www.instagram.com/accounts/login/')
 time.sleep(2)
 username = browser.find_element_by_xpath('//input[@name="username"]')
-username.send_keys('###########')
+username.send_keys('###########')	#username 
 password = browser.find_element_by_xpath('//input[@name="password"]')
-password.send_keys('############')
+password.send_keys('############')	# password
 logIn = browser.find_element_by_css_selector('._qv64e')
 logIn.click()
 time.sleep(5)
 count=0
 L = ['asmm','eppic','wonderland','woooow','cooool','wonderful','kepitup','good','perfect click','beaches']
 while count < 10:
-	browser.get("https://www.instagram.com/p/BeFXzKjDPwM/?taken-by=dm_ur_creativity");
+	browser.get("https://www.instagram.com/p/BeFXzKjDPwM/?taken-by=dm_ur_creativity");	# Sample post link
 	browser.execute_script("window.scrollTo(0, 800)") 
 	open_comment_section(browser)
 	comment_input = get_comment_input(browser)
